a012ee46b87217c030337a4145624de8f7dc8464
nichapat-t <ijang2547@gmail.com>
2025-04-25 21:51:54 +0700

    import animate

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,6 +17,34 @@ export default {
         //   200: "#B58399",
         // },
       },
+      keyframes: {
+        headShake: {
+          "0%": {
+            transform: "translateX(0)",
+          },
+          "6.5%": {
+            transform: "translateX(-6px) rotateY(-9deg)",
+          },
+
+          "18.5%": {
+            transform: "translateX(5px) rotateY(7deg)",
+          },
+
+          "31.5%": {
+            transform: "translateX(-3px) rotateY(-5deg)",
+          },
+
+          "43.5%": {
+            transform: "translateX(2px) rotateY(3deg)",
+          },
+          "50%": {
+            transform: "translateX(0)",
+          },
+        },
+      },
+      animation: {
+        headShake: "headShake 0.5s 0.5",
+      },
     },
     screens: {
       "2xl": { max: "1535px" },
